6fa40edae471c6e15271835293bbda0e358ea392
Jharna Khatun <jharnakhatun203@gmail.com>
2024-11-12 09:58:54 +0600

    generate random alphabet and set key background key

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,15 @@
 const playBtn = element('.homeBtn');
+
+function continueGame(){
+    const alphabet = getRandomAlphabet();
+    const currentAlphabet = elementById('current-alphabet');
+    currentAlphabet.textContent = alphabet;
+    setBackgroundColor(alphabet);
+}
+
 function play(){
     hideElement('.home');
-    showElement('.playground')
+    showElement('.playground');
+    continueGame();
 }
 playBtn.addEventListener('click', play);
--- src/script/utility.js
+++ src/script/utility.js
@@ -1,5 +1,6 @@
 // Select element
 const element = (selector) => document.querySelector(selector);
+const elementById = (id) => document.getElementById(id);
 // Hide element
 const hideElement = (selector) =>{
     const element = document.querySelector(selector);
@@ -9,4 +10,23 @@ const hideElement = (selector) =>{
 const showElement = (selector) =>{
     const element = document.querySelector(selector);
     element.classList.remove('hidden')
+}
+//random alphabet
+const getRandomAlphabet = () =>{
+    //create alphabet array
+    const alphabetStrings = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetStrings.split('');
+    //crate random index
+    const randomIndex = Math.random()*25;
+    const index = Math.round(randomIndex);
+    //get alphabet index
+    const alphabet = alphabets[index];
+    return alphabet;
+    
+}
+
+//set Background color on key
+const setBackgroundColor = (id)=>{
+    const element = elementById(id);
+    element.classList.add('bg-orange-400', 'text-black');
 }
